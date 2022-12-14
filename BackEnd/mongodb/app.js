50fb2f666427b99f0e377227f6fb7d321ece6ce4
jmahirakz <jmahirakz@paypal.com>
2022-12-14 23:07:15 +0530

    Node Project Structure

---
+++ BackEnd/mongodb/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const movieRouter = require('./routes/movieRouter')
+
+const app = express()
+
+//middleware - Parsing request body which is in JSON Format
+app.use(express.json())
+app.use('/movies', movieRouter)
+app.use('/users', userRouter)
+
+app.post
+
+app.listen(8000, () => {
+  console.log("Started Successfully")
+})
---
+++ BackEnd/mongodb/controllers/movieController.js
@@ -0,0 +1,29 @@
+const getMovies = (req, res) => {
+
+}
+
+const getMoviesByID = (req, res) => {
+
+
+}
+
+const postMovie = (req, res) => {
+
+
+}
+
+const updateMovieById = (req, res) => {
+
+}
+
+const deleteMovieByID = (req, res) => {
+
+}
+
+module.exports = {
+  getMovies,
+  getMoviesByID,
+  postMovie,
+  updateMovieById,
+  deleteMovieByID
+}
